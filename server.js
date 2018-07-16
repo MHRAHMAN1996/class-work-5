997d8ecdb89d88eb201f50090fd76d5995750ede
Md Hasibur Rahman <mhrahman1996@gmail.com>
2018-07-16 04:47:12 +0000

    class-work-7

--- server.js
+++ server.js
@@ -2,13 +2,19 @@ var http = require('http');
 var express=require("express");
 var app=express();
 var server=http.Server(app);
-app.get ('/', function(req,res){
+
+app.get ('/index', function(req,res){
   res.sendfile(__dirname+'/index.html');
 });
+
 app.get ('/about', function(req,res){
   res.sendfile(__dirname+'/about.html');
 });
+
+app.get ('/form', function(req,res){
+  res.sendfile(__dirname+'/form.html');
+});
+
  server.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server running');
-
-  });
+});
