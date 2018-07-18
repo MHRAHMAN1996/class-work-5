e92de2fadbe78e6bd11520aba4740bf27834241c
Md Hasibur Rahman <mhrahman1996@gmail.com>
2018-07-18 04:39:03 +0000

    class-work-7

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
 var http = require('http');
 var express=require("express");
 var app=express();
+var bodyParser= require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extend:true}));
 var server=http.Server(app);
 
-app.get ('/index', function(req,res){
-  res.sendfile(__dirname+'/index.html');
+app.get ('/', function(req,res){
+  res.sendFile(__dirname+'/index.html');
 });
 
 app.get ('/about', function(req,res){
-  res.sendfile(__dirname+'/about.html');
+  res.sendFile(__dirname+'/about.html');
+});
+
+app.get ('/about', function(req,res){
+  res.sendFile(__dirname+'/form.html');
 });
 
 app.get ('/form', function(req,res){
-  res.sendfile(__dirname+'/form.html');
-  
-  
+  res.sendFile(__dirname+'/form.html');
+});
+
+app.get ('/submit_user', function(req,res){
+  console.log(req.body);
+});
+
+app.get ('/system/about', function(req,res){
+  res.sendFile(__dirname+'/about.html');
 });
 
  server.listen(process.env.PORT, process.env.IP, function(){
