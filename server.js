67979e23094b1cfe054bc9f717a8578c30e32672
Md Hasibur Rahman <mhrahman1996@gmail.com>
2018-07-30 04:44:53 +0000

    classwork9

--- server.js
+++ server.js
@@ -1,54 +1,72 @@
 var http = require('http');
-var mongo=require('mongodb');
-  var express = require('express');
-  var app = express();
-  var bodyParser = require('body-parser');
- 
-  
-var db, uri = "mongodb://" + process.env.IP + "/test";
-   
-  mongo.MongoClient.connect(uri,{userNewUrlParser:true},function(err,client){
-    if(err){console.log("Could not connect to MongoDB")
-    }
-    else {
-      db = client.db('simple-node');
-    }
-  });
-   
-  var save = function(form_data){
-    db.createCollection('users',function(err,collection){ if (err) throw err;});
-    var collection = db.collection('users');
-    collection.save(form_data);
-  }
-  
- var server = http.Server(app);
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({extended:true}));
-  
-  app.get('/', function(req, res){
-    res.sendFile(__dirname+'/index.html');
-  })
-  
-  app.get('/about', function(req, res){
-    res.sendFile(__dirname+'/about.html');
-  });
-  
-   app.get('/form', function(req, res){
-    res.sendFile(__dirname+'/form.html');
-  });
-  
-  app.post('/signup', function(req, res) {
-    var username = req.body.username;
-    var email = req.body.email;
-    console.log("post received: %s %s", username, email);
-});
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+var server = http.Server(app);
+//var mongo = require('mongodb');
+var mongoose = require('mongoose');
+mongoose.connect("mongodb://" + process.env.IP + ":27017/test");
 
-app.post('/submit_user',function(req, res){
-  console.log(req.body);
-   save(req.body);
-  res.status('200');
+mongoose.connection.on('erroe',function(){
+  console.log('Could not connect to mongodb');
 });
+//define schema with mongoose
+var userSchema = new mongoose.Schema({
+  name:{
+    type: String,
+    required: "Name is required"
+  } ,
+  email:String
+});
+var User = mongoose.model('User',userSchema);
+//for local
+//var db, url = "mongodb://localhost:27071";
+//for c9
+//var db, url = 'mongodb://' + process.env.IP + ":27017/test";
+//mongo.MongoClient.connect(url,
+ //           {useNewUrlParser:true},
+ //           function(err,client){
+ //             if(err){
+  //              console.log(err);
+  //              console.log("Could not connect to mongoDB");
+ //             }else{
+ //               db= client.db('node=cwD');
+//              }
+//            });
+//            
+//var save = function(from_data){
+ // db.createCollection('users',function(err, collection){});
+//  var collection= db.collection('users');
+//  collection.save(from_data);
+//};            
 
+
+
+app.get('/',function(req,res){
+  res.sendFile(__dirname+'/index.html');
+});
+app.get('/about',function(req,res){
+  res.sendFile(__dirname+'/about.html');
+});
+app.get('/form',function(req,res){
+  res.sendFile(__dirname+'/form.html');
+});
+app.post('/submit_user',function(req,res)
+{
+    console.log(req.body);
+    var new_user = new User(req.body);
+    new_user.save(function(err,data){
+      if(err){
+        return res.status(400)
+                  .json({message:"couldn't save user"})
+      }
+      res.status(200).json(data);
+    })
+    //save(req.body);
+    res.status(200);
+});
   server.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server running');
 });
